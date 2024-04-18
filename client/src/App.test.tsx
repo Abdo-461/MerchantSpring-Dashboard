f67ebbd01688ea3d0961fdb1d3c6696849c04963
Abdulrahman <abdulrahmanmvp93@gmail.com>
2024-04-18 17:05:07 +1000

    Add front-end test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,6 +1,8 @@
 import App from './App';
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, wait, waitForElement } from '@testing-library/react';
+
+
 
 test('renders app header', () => {
   const { getByText } = render(<App />);
@@ -12,4 +14,16 @@ test('renders user name', () => {
   const { getByText } = render(<App />);
   const headerText = getByText("Welcome, Guest!");
   expect(headerText).toBeInTheDocument();
+});
+
+test('Check first loading message appears to ensure data fetching started', () => {
+  const { getByText } = render(<App />);
+  const loadingMessage = getByText("Loading...");
+  expect(loadingMessage).toBeInTheDocument();
+});
+
+test('Check second loading message appears to ensure data fetching started', () => {
+  const { getByText } = render(<App />);
+  const loadingMessage = getByText("This might take a while. Hang on!");
+  expect(loadingMessage).toBeInTheDocument();
 });
