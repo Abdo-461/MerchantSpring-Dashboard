a4714a7021d6dc4cd6d90bc2e1fe0624ec08dd4a
Abdulrahman <abdulrahmanmvp93@gmail.com>
2024-04-18 14:36:09 +1000

    Add helper methods for column sorting.

--- client/src/components/helper.tsx
+++ client/src/components/helper.tsx
@@ -4,11 +4,11 @@
 // a method to calculate days overdue
 export function calculateDaysOverdue(latestShipDate: Date): number {
 
-    const dateToday = new Date ();
+    const dateToday = new Date();
     const shipDate = convertDateFormat(latestShipDate.toString());
     let differenceInTime = dateToday.getTime() - new Date(shipDate).getTime();
-    let differenceInDays = Math.round(differenceInTime/ (1000 * 3600 * 24));
-    
+    let differenceInDays = Math.round(differenceInTime / (1000 * 3600 * 24));
+
     return differenceInDays;
 }
 
